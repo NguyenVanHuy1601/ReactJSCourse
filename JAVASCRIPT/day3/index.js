366f0fe6071dda8998f6546ce6a1b4ebc261085e
Nguyen Van Huy <16012002h@gmail.com>
2022-02-18 16:35:24 +0700

    push day 3

--- JAVASCRIPT/day3/index.js
+++ JAVASCRIPT/day3/index.js
@@ -1,1 +1,40 @@
-alert("xin chào");
+// alert("xin chào");
+
+
+function hello(studentName, universityName) {
+    if (studentName && universityName) {
+        console.log("hello Tam");
+        console.log("My name is", studentName, ", university: ", universityName);
+        return true;
+    }
+    return false;
+}
+hello("Huy", "Buu Chinh");
+
+hello("Tam", "KHXH&NV");
+let check = hello("", "Buu chinh");
+if (!check) {
+    console.log("something went wrong on print");
+}
+
+// write a function check is a number prime?
+function checkPrime(number) {
+    let check = true;
+    if (number < 2) {
+        return false;
+    } else {
+        for (let i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+let aNumber = window.prompt("enter a number to check ");
+let checkP = checkPrime(aNumber);
+if (checkP) {
+    console.log(aNumber, "is Prime number");
+} else {
+    console.log(aNumber, "is Not Prime number");
+}
